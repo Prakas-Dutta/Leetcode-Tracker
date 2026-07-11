70f225995f33c674963787b0d34c5c74998394bc
Prakas-Dutta <prakasdutta7@gmail.com>
2026-07-11 21:34:39 +0530

    Convert returned result to dictionary type

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from fastapi import FastAPI
 from database import conn
-from model import Problem
+from model import Problem, completedProblem
 
 app = FastAPI()
 
 @app.get('/{id}')
-def get_database_info(id:int):
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM problem_list WHERE id = %d'%(id))
+def get_problem_info(id: int):
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM problem_list WHERE id = %s', (id,))
     problem = cursor.fetchone()
-    if problem == None:
-        return 'Enter valid leetcode ID!!!'
+    if problem is None:
+        return "Enter valid LeetCode ID!!!"
     return problem
 
 @app.post('/')
-def add_problem(problem_info: Problem):
+def add_problem(problem_info: completedProblem):
     problem = problem_info.model_dump()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM problem_list WHERE id = %d'%(problem['leetcode_id']))
     db_problem = cursor.fetchone()
-    if db_problem != None:
+    if db_problem == None:
         cursor.close()
         return "Wrong information!!!"
     else:
-        cursor.execute('INSERT INTO completed_list VALUES(%s, %s, %s, %s)'%(problem['leetcode_id'], problem['title'], problem['difficulty'], problem['approach']))
+        cursor.execute('INSERT INTO completed_list VALUES(%s, %s, %s, %s)', (problem['leetcode_id'], problem['title'], problem['difficulty'], problem['approach']))
         conn.commit()
         cursor.close()
+        return "Successfully added!!!"
+
+@app.get('/no_of_problem/')
+def no_of_problem():
+    cursor = conn.cursor(buffered=True)
+    cursor.execute('SELECT COUNT(*) FROM problem_list')
+    number = cursor.fetchone()
+    cursor.close()
+    return number[0]
+
+@app.get('/list_problem_approach/')
+def list_problem_approach():
+    cursor = conn.cursor(buffered=True, dictionary=True)
+    cursor.execute('SELECT DISTINCT approach, COUNT(*) FROM completed_list GROUP BY approach')
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
+
--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel
 
-class Problem(BaseModel):
+class completedProblem(BaseModel):
+        id: int
         leetcode_id: int
         title: str
         difficulty: str
         approach: str
+
+class Problem(BaseModel):
+        leetcode_id: int
+        title: str
+        difficulty: str

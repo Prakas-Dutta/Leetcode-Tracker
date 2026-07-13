79785e1649740bc4c0a920a27055f7b0d3d6b5b9
Prakas-Dutta <prakasdutta7@gmail.com>
2026-07-13 12:56:25 +0530

    Add delete, update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import conn
-from model import Problem, completedProblem
+from model import CompletedProblem, UpdatedInfo
 
 app = FastAPI()
 
@@ -9,26 +9,27 @@ def get_problem_info(id: int):
     cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT * FROM problem_list WHERE id = %s', (id,))
     problem = cursor.fetchone()
+    cursor.close()
     if problem is None:
-        return "Enter valid LeetCode ID!!!"
+        raise HTTPException(status_code=404, detail='ID is not valid')
     return problem
 
 @app.post('/')
-def add_problem(problem_info: completedProblem):
+def add_problem(problem_info: CompletedProblem):
     problem = problem_info.model_dump()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM problem_list WHERE id = %d'%(problem['leetcode_id']))
+    cursor.execute('SELECT * FROM problem_list WHERE id = %s', (problem['leetcode_id'],))
     db_problem = cursor.fetchone()
     if db_problem == None:
         cursor.close()
-        return "Wrong information!!!"
+        raise HTTPException(status_code=404, detail='Wrong information')
     else:
-        cursor.execute('INSERT INTO completed_list VALUES(%s, %s, %s, %s)', (problem['leetcode_id'], problem['title'], problem['difficulty'], problem['approach']))
+        cursor.execute('INSERT INTO completed_list(leetcode_id, title, difficulty, approach) VALUES(%s, %s, %s, %s)', (problem['leetcode_id'], problem['title'], problem['difficulty'], problem['approach'],))
         conn.commit()
         cursor.close()
         return "Successfully added!!!"
 
-@app.get('/no_of_problem/')
+@app.get('/problem_list/')
 def no_of_problem():
     cursor = conn.cursor(buffered=True)
     cursor.execute('SELECT COUNT(*) FROM problem_list')
@@ -36,7 +37,7 @@ def no_of_problem():
     cursor.close()
     return number[0]
 
-@app.get('/list_problem_approach/')
+@app.get('/completed_list/')
 def list_problem_approach():
     cursor = conn.cursor(buffered=True, dictionary=True)
     cursor.execute('SELECT DISTINCT approach, COUNT(*) FROM completed_list GROUP BY approach')
@@ -44,4 +45,33 @@ def list_problem_approach():
     cursor.close()
     return result
 
+@app.delete('/completed_list/{id}/{approach}')
+def delete_completed_problem(id:int, approach:str):
+    cursor = conn.cursor(buffered=True)
+    cursor.execute('SELECT * FROM completed_list WHERE leetcode_id=%s and approach=%s', (id, approach, ))
+    row = cursor.fetchone()
+    if row:
+        cursor.execute('DELETE FROM completed_list WHERE leetcode_id=%s and approach=%s', (id, approach, ))
+        conn.commit()
+        cursor.close()
+        return 'Deleted successfully!!!'
+    else:
+        cursor.close()
+        raise HTTPException(status_code=404, detail='The value of ID or approach is wrong')
+
+
+@app.patch('/completed_list/')
+def update_approach(problem_info:UpdatedInfo):
+    problem = problem_info.model_dump()
+    cursor = conn.cursor(buffered=True)
+    cursor.execute('SELECT * FROM completed_list WHERE leetcode_id=%s and approach=%s', (problem['id'], problem['approach'], ))
+    row = cursor.fetchone()
+    if row:
+        cursor.execute('UPDATE completed_list SET approach=%s WHERE leetcode_id=%s and approach=%s', (problem['updated_approach'], problem['id'], problem['approach'], ))
+        conn.commit()
+        cursor.close()
+        return 'Updated successfully!!!'
+    else:
+        cursor.close()
+        raise HTTPException(status_code=404, detail='The value of ID or approach is wrong')
 

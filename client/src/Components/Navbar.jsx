d2bf01119c1e3f9164c49d397f3b7ad4b749258e
Prakas Dutta <prakasdutta7@gmail.com>
2026-08-03 14:41:07 +0530

    Fix navbar routing problem

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,20 +1,21 @@
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/home">LeetiBuddy</a>
+      <Link className="navbar-brand" to="/home">LeetiBuddy</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-link active" aria-current="page" href="/home">Home</a>
-          <a className="nav-link" href="/add">Add Problem</a>
-          <a className="nav-link" href="/delete">Delete Problem</a>
-          <a className="nav-link" href="/update">Update Problem</a>
-          <a className="nav-link" href="/performance">Performance</a>
-          <a className="nav-link" href="/suggestions">Suggestions</a>
+          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link" to="/add">Add Problem</Link>
+          <Link className="nav-link" to="/delete">Delete Problem</Link>
+          <Link className="nav-link" to="/update">Update Problem</Link>
+          <Link className="nav-link" to="/performance">Performance</Link>
+          <Link className="nav-link" to="/suggestions">Suggestions</Link>
         </div>
       </div>
     </div>

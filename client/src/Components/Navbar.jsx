70d460b0a687fe3416cdfb256d7a98c7fb43c55b
Prakas Dutta <prakasdutta7@gmail.com>
2026-08-05 10:56:31 +0530

    Fix navbar wrong route problem

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,13 +4,13 @@ function Navbar() {
     return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/home">LeetiBuddy</Link>
+      <Link className="navbar-brand" to="/">LeetiBuddy</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           <Link className="nav-link" to="/add">Add Problem</Link>
           <Link className="nav-link" to="/delete">Delete Problem</Link>
           <Link className="nav-link" to="/update">Update Problem</Link>
